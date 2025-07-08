bb4c6bed54c0806fc28f5d2f5a00caa1379f0f13
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-07-08 14:53:31 -0400

    Refactor: new methods for the config.go functions

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,19 +6,34 @@ import (
 	"path/filepath"
 )
 
-type Printer struct {
+type ConfigItem struct {
 	Name    string
 	Command string
 }
 
-var printers = map[int]Printer{
+var printers = map[int]ConfigItem{
 	1: {"Salita", "lpr -P hp-335"},
 	2: {"Toqui", "lpr"},
 }
 
+var modes = map[int]ConfigItem{
+	1: {"Simple", ""},
+	2: {"Doble", "duplex"},
+}
+
+var borders = map[int]ConfigItem{
+	1: {"Corto", "duplex -l"},
+	2: {"Largo", "duplex"},
+}
+
+// Todo: support -dFirstPage= y -dLastPage= from postscript (or psselect -p5-10)
+// Todo: Consultar papel?
 type Config struct {
 	Theme   string `json:"theme"`
 	Account string `json:"account"`
+	Printer string `json:"printer"`
+	Mode    string `json:"mode"`
+	Border  string `json:"border"`
 }
 
 func configPath() (string, error) {
@@ -30,21 +45,22 @@ func configPath() (string, error) {
 }
 
 func Load() Config {
+	defaultConfig := Config{Theme: "Default", Account: "", Printer: "Toqui", Mode: "Doble", Border: "Largo"}
 	path, err := configPath()
 	if err != nil {
-		return Config{Theme: "Default", Account: ""}
+		return defaultConfig
 	}
 
 	file, err := os.Open(path)
 	if err != nil {
-		return Config{Theme: "Default", Account: ""}
+		return defaultConfig
 	}
 	defer file.Close()
 
 	var cfg Config
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&cfg); err != nil {
-		return Config{Theme: "Default", Account: ""}
+		return defaultConfig
 	}
 
 	if cfg.Theme == "" {
@@ -71,14 +87,28 @@ func save(cfg Config) error {
 	return encoder.Encode(cfg)
 }
 
-func SaveTheme(theme string) error {
+func updateConfig(updater func(cfg *Config)) error {
 	cfg := Load()
-	cfg.Theme = theme
+	updater(&cfg)
 	return save(cfg)
 }
 
+func SaveTheme(theme string) error {
+	return updateConfig(func(cfg *Config) { cfg.Theme = theme })
+}
+
 func SaveAccount(account string) error {
-	cfg := Load()
-	cfg.Account = account
-	return save(cfg)
+	return updateConfig(func(cfg *Config) { cfg.Account = account })
+}
+
+func SavePrinter(printer string) error {
+	return updateConfig(func(cfg *Config) { cfg.Printer = printer })
+}
+
+func SaveMode(mode string) error {
+	return updateConfig(func(cfg *Config) { cfg.Mode = mode })
+}
+
+func SaveBorder(border string) error {
+	return updateConfig(func(cfg *Config) { cfg.Border = border })
 }
